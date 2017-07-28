612556a5a330cb2b7137c7d07da580927b2f140b
Amitesh Kumar <amitesh.kumar@mastercard.com>
2017-07-28 17:28:28 +0530

    Adding sample csv file download button

--- src/components/import/import.controller.js
+++ src/components/import/import.controller.js
@@ -13,6 +13,10 @@ export default function importController($scope, $timeout, uiGridImporterService
         $('#import-file').click();
     };
 
+    vm.downloadSampleCSV = function () {
+        return downloadExcelCsv();
+    };
+
     vm.gridOptions = {
         data: [],
         importerDataAddCallback: function (grid, newObjects) {
@@ -31,7 +35,7 @@ export default function importController($scope, $timeout, uiGridImporterService
 
         if (file.files[0]) {
             vm.reset();
-            $timeout(function(){
+            $timeout(function () {
                 reader.readAsText(file.files[0]);
             }, 10);
         } else {
@@ -44,4 +48,56 @@ export default function importController($scope, $timeout, uiGridImporterService
         vm.gridOptions.columnDefs = [];
         vm.gridOptions.columns = null;
     }
+
+    // Download sample file logic
+
+     function downloadExcelCsv() {
+        var attachmentFilename = 'sample-candidate-list.csv';
+        var rows = [['drive name','scheduled on','firstname','lastname','email','phone','stream','role']];
+
+        function asUtf16(str) {
+            var buffer, bufferView, i, j, ref, val;
+            buffer = new ArrayBuffer(str.length * 2);
+            bufferView = new Uint16Array(buffer);
+            bufferView[0] = 0xfeff;
+            for (i = j = 0, ref = str.length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+                val = str.charCodeAt(i);
+                bufferView[i + 1] = val;
+            }
+            return bufferView;
+        }
+
+        function makeExcelCsvBlob(rows) {
+            return new Blob([asUtf16(toTsv(rows)).buffer], {
+                type: "text/csv;charset=UTF-16"
+            });
+        }
+
+        function toTsv(rows) {
+            var escapeValue;
+            escapeValue = function (val) {
+                if (typeof val === 'string') {
+                    return '"' + val.replace(/"/g, '""') + '"';
+                } else if (val != null) {
+                    return val;
+                } else {
+                    return '';
+                }
+            };
+            return rows.map(function (row) {
+                    return row.map(escapeValue).join('\t');
+                }).join('\n') + '\n';
+        }
+
+        var a, blob;
+        blob = makeExcelCsvBlob(rows);
+        a = document.createElement('a');
+        a.style.display = 'none';
+        a.download = attachmentFilename;
+        document.body.appendChild(a);
+        a.href = URL.createObjectURL(blob);
+        a.click();
+        URL.revokeObjectURL(a.href);
+        a.remove();
+    };
 }
